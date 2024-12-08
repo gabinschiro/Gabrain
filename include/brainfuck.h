68f9791ada8291a64642a6fcf35dbdba9736f1db
zqodev <gabin.schiro@epitech.eu>
2024-12-08 16:38:23 +0100

    feat: add @ for reset value, ~ for reverse binary and ! to exit the program

--- include/brainfuck.h
+++ include/brainfuck.h
@@ -1,6 +1,12 @@
 #ifndef BRAINFUCK_H
     #define BRAINFUCK_H
 
+typedef enum return_code_e {
+    EXIT,
+    ERROR,
+    SUCCESS
+} return_code_t;
+
 int interpret_brainfuck(const char *file_name);
 
 #endif
--- src/brainfuck.c
+++ src/brainfuck.c
@@ -1,23 +1,24 @@
+#include "brainfuck.h"
 #include "util.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static int interpret_char(unsigned char **current, const char c,
+static return_code_t interpret_char(unsigned char **current, const char c,
     unsigned char *memory_table, const size_t memory_size)
 {
     switch (c) {
         case '>':
             if (*current >= memory_table + memory_size) {
                 fprintf(stderr, "Memory out of bounds.\n");
-                return (1);
+                return (ERROR);
             }
             (*current)++;
             break;
         case '<':
             if (*current <= memory_table) {
                 fprintf(stderr, "Memory out of bounds.\n");
-                return (1);
+                return (ERROR);
             }
             (*current)--;
             break;
@@ -42,10 +43,18 @@ static int interpret_char(unsigned char **current, const char c,
             if (**current >= 'A' && **current <= 'Z')
                 **current += 32;
             break;
+        case '@':
+            **current = 0;
+            break;
+        case '~':
+            **current = ~**current;
+            break;
+        case '!':
+            return (EXIT);
         default:
             break;
     }
-    return (0);
+    return (SUCCESS);
 }
 
 int interpret_brainfuck(const char *file_name)
@@ -56,6 +65,7 @@ int interpret_brainfuck(const char *file_name)
     unsigned char *current;
     size_t program_counter = 0;
     size_t program_length;
+    return_code_t result;
 
     if (memory_table == NULL) {
         fprintf(stderr, "Failed to allocate memory.\n");
@@ -70,11 +80,14 @@ int interpret_brainfuck(const char *file_name)
     }
     program_length = strlen(program);
     while (program_counter < program_length) {
-        if (interpret_char(&current, program[program_counter], memory_table, memory_size)) {
+        result = interpret_char(&current, program[program_counter], memory_table, memory_size);
+        if (result == ERROR) {
             free(memory_table);
             free(program);
             return (1);
         }
+        if (result == EXIT)
+            break;
         program_counter++;
     }
     free(memory_table);
